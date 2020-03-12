dfa9242bc529a639efb841d28734cbcd8b0bdccf
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-03-12 05:11:39 -0400

    :bug: Fix git bug.

--- source/git.m
+++ source/git.m
@@ -5,31 +5,31 @@ parametersList = split(parameterStr);
 firstParameter = parametersList{1};
 gitPlusParameters = ['git ' parameterStr];
 switch firstParameter
-case 'checkout'
-case 'rev-parse'
-case '--help'
-case 'merge-base'
+case 'remote'
+case 'pull'
+case 'fetch'
+otherwise
     [status, result] = system(gitPlusParameters);
     status = ~status;
-otherwise
-    name = getRepoName();
-    filename = [name 'CommandOutput'];
-    fid = fopen(filename , 'wt' );
-    fclose(fid);
-
-    [status, ~] = dos([which('RunCommand.bat') ' ' name ' ' gitPlusParameters ' &']);
-    while(true)
-        fid = fopen(filename, 'rt' );
-        contents = fscanf(fid, '%s\n');
-        fclose(fid);
-        if ~isempty(contents)
-            delete(filename)
-            break;
-        end
-        pause(0.2);
-    end
-    result = contents;
+    return;
 end
+name = getRepoName();
+filename = [name 'CommandOutput'];
+fid = fopen(filename , 'wt' );
+fclose(fid);
+
+[status, ~] = dos([which('RunCommand.bat') ' ' name ' ' gitPlusParameters ' &']);
+while(true)
+    fid = fopen(filename, 'rt' );
+    contents = fscanf(fid, '%s\n');
+    fclose(fid);
+    if ~isempty(contents)
+        delete(filename)
+        break;
+    end
+    pause(0.2);
+end
+result = contents;
 
 function name = getRepoName()
 [~, result] = system('git rev-parse --show-toplevel');
