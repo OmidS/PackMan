83c33aa8d0c4dc57059c0ef91a142c67764fffa9
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-03-12 04:47:05 -0400

    :rocket: Speed up git commands when auth is not needed.

--- source/git.m
+++ source/git.m
@@ -1,25 +1,34 @@
 function [status, result] = git(parameterStr)
 %GIT Summary of this function goes here
 %   Detailed explanation goes here
-name = getRepoName();
-filename = [name 'CommandOutput'];
-fid = fopen(filename , 'wt' );
-fclose(fid);
-
-[status, ~] = dos([which('RunCommand.bat') ' ' name ' git ' parameterStr ' &']);
-% [status, result] = system(['pwsh -Command .\RunCommand.ps1 git ' parameterStr ' &']);
-while(true)
-    fid = fopen(filename, 'rt' );
-    contents = fscanf(fid, '%s\n');
+parametersList = split(parameterStr);
+firstParameter = parametersList(1);
+gitPlusParameters = ['git ' parameterStr];
+switch firstParameter
+case 'checkout'
+case 'rev-parse'
+case '--help'
+case 'merge-base'
+    [status, result] = system(gitPlusParameters);
+otherwise
+    name = getRepoName();
+    filename = [name 'CommandOutput'];
+    fid = fopen(filename , 'wt' );
     fclose(fid);
-    if ~isempty(contents)
-        delete(filename)
-        break;
+
+    [status, ~] = dos([which('RunCommand.bat') ' ' name ' ' gitPlusParameters ' &']);
+    while(true)
+        fid = fopen(filename, 'rt' );
+        contents = fscanf(fid, '%s\n');
+        fclose(fid);
+        if ~isempty(contents)
+            delete(filename)
+            break;
+        end
+        pause(0.2);
     end
-    pause(0.2);
+    result = contents;
 end
-result = contents;
-% fclose(fid);
 
 function name = getRepoName()
 [~, result] = system('git rev-parse --show-toplevel');
