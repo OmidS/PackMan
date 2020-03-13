203b19e06039bb345c890046330aa12e3a7b8337
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-03-13 01:56:42 -0400

    :art: Improve code quality.

--- source/PackMan.m
+++ source/PackMan.m
@@ -290,12 +290,13 @@ classdef PackMan < handle & matlab.mixin.Copyable
             pm = PackMan([], '', '', depDir);
             
             if (~strcmp(dep.Name,'PackMan'))
-                oldPath = path;
                 generatedPath = pm.genPath;
                 generatedPathWithoutLastSemicolon = generatedPath(1:end-1);
                 genPaths = split(generatedPathWithoutLastSemicolon, ';');
                 if (all(cellfun(@exist, genPaths)== 7))
+                    oldPath = path;
                     addpath(pm.genPath);
+                    path(oldPath);
                     s = which('installDeps.m', '-ALL');
                     pathIndexesContainintDepDir = contains(s, depDir);
                     if any(pathIndexesContainintDepDir)
@@ -304,8 +305,6 @@ classdef PackMan < handle & matlab.mixin.Copyable
                         getDepListFunction = fullfile(depDir, dpDirPth, 'getDepList.m');
                         run(getDepListFunction);
                         pm = PackMan(ans, fullfile(fileparts(installDepsPath),'external') , '', depDir);
-                    else
-                        path(oldPath);
                     end
                 end
             end
